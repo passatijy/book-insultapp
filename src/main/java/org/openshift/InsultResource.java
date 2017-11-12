d19461de9652d5c2eb927da5c52f458ba5191639
mum1 <mum1>
2017-11-12 20:40:20 +0300

    InsultResource change insLult to insult

--- src/main/java/org/openshift/InsultResource.java
+++ src/main/java/org/openshift/InsultResource.java
@@ -7,13 +7,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 @RequestScoped
-@Path("/inslult")
+@Path("/insult")
 public class InsultResource {
 	@GET()
 	@Produces("application/json")
 	public HashMap<String,String> getInsult() {
 		HashMap<String,String> theInsult = new HashMap<String,String>();
-		theInsult.put("inslult", new InsultGenerator().generateInsult());
+		theInsult.put("insult", new InsultGenerator().generateInsult());
 		return theInsult;
 	}
 }
